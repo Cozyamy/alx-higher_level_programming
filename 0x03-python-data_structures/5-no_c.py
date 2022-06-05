fa3dbe1b60698067fc54d0f80e0960dce513c2e4
Cozyamy <cozyamy@gmail.com>
2022-06-05 18:07:31 +0100

    Data Structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for c in my_string:
-        if a.lower() != "c":
-            new_string += c
-    return new_string
+
+    replace = ""
+
+    for a in my_string:
+        if a != 'C' and a != 'c':
+            replace += a
+
+    return (replace)
