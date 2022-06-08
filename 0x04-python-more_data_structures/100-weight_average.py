c01594ee4edaa0abd290b2eb05cb521a5b219ba0
Cozyamy <cozyamy@gmail.com>
2022-06-08 22:07:30 +0100

    More on data structures of python

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """Return the weighted average of all integers tuple (<score>, <weight>)"""
+
+    aver = 0
+
+    if my_list:
+        aver = sum([i[0] * i[1] for i in my_list]) / sum([i[1] for i in my_list])
+
+    return aver
