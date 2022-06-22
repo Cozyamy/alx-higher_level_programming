5b4e5d4f5317c7f88c63aa303f7b200fc6be284f
Cozyamy <cozyamy@gmail.com>
2022-06-22 01:03:52 +0100

    3-square.py is now executable

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
+
+"""Defines a square."""
+
+
 class Square:
-    """Square Class
-    A Square Class
-    """
+    """Representation of Square class"""
 
     def __init__(self, size=0):
-        """__init__
-        The __init__ method initializes the size value of the square.
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
-        Raises:
-            TypeError: If `size` type is not `int`.
-            ValueError: If `size` is less than `0`.
-        """
-
         if type(size) is not int:
-            raise TypeError('size must be an integer')
-
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.__size = size
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
-        """Returns the current square area
-        """
         return self.__size ** 2
