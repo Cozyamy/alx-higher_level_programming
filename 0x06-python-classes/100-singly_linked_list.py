1348b401042a716d0555806d81667e5ad02e334a
Cozyamy <cozyamy@gmail.com>
2022-06-22 01:32:43 +0100

    100-singly_linked_list.py is now executable

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,5 +1,10 @@
-#!/usr/python3
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
 class Node:
+    """Representation of a square"""
+
     def data(self):
         pass
     def next_node(self):
